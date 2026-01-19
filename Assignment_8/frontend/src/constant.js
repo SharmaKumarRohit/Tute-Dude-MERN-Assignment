38995e00f696281b99393435048aac44238673ef
SharmaKumarRohit <sharmaeditx375@gmail.com>
2026-01-19 08:21:59 +0530

    adding live url of backend

--- Assignment_8/frontend/src/constant.js
+++ Assignment_8/frontend/src/constant.js
@@ -1,1 +1,2 @@
-export const BASE_URI = "http://localhost:4000/api";
+// export const BASE_URI = "http://localhost:4000/api";
+export const BASE_URI = "https://tute-dude-mern-assignment.onrender.com/api";
