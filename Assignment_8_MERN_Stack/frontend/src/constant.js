9d4c9d2f484db3336855a7b702b91a0ac674e999
SharmaKumarRohit <sharmaeditx375@gmail.com>
2026-02-02 18:02:05 +0530

    modifying deployment link

--- Assignment_8_MERN_Stack/frontend/src/constant.js
+++ Assignment_8_MERN_Stack/frontend/src/constant.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:4000/api/todos";
-export const BASE_URL = "https://tute-dude-mern-assignment-1.onrender.com";
+export const BASE_URL =
+  "https://tute-dude-mern-assignment-1.onrender.com/api/todos";
